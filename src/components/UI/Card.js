a7273c5aae2e68d8b455af77f1ec855d6e78c54a
Patricio <patricioguerramarquez@gmail.com>
2022-03-13 13:17:59 +0100

    Static components and props

---
+++ src/components/UI/Card.js
@@ -0,0 +1,9 @@
+//Shell component
+import './Card.css';
+
+const Card = (props) => {
+  const classes = 'card ' + props.className;
+  return <div className={classes}>{props.children}</div>
+}
+
+export default Card;
